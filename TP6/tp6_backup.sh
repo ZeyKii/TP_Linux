068753e13da91244729d751fd606ae4a56c7e73d
ZeyKii <max.ferran@hotmail.com>
2023-02-12 21:03:20 +0100

    FIX

--- TP6/tp6_backup.sh
+++ TP6/tp6_backup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# This script was written by Lucas Hanson on the 6th of January 2023.
-# The script backs up the most important files of the nextcloud folder by archiving the themes, data and config files, and placing the backup in the /srv/backup folder
+# The script backs places the backup in the /srv/backup folder
 
 [[ -d /srv/backup ]] || mkdir /srv/backup
 dnf install tar -y
