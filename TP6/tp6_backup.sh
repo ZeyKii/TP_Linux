f90b9363cb18baa3b20482d2c05123936520b1e6
ZeyKii <max.ferran@hotmail.com>
2023-02-12 21:06:16 +0100

    FIX

--- TP6/tp6_backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# The script backs up places the backup in the /srv/backup folder
-
-[[ -d /srv/backup ]] || mkdir /srv/backup
-dnf install tar -y
-cd /var/www/tp5_nextcloud/
-tar -czf /srv/backup/nextcloud_$( date '+%Y-%m-%d_%H-%M-%S' ).tar.gz themes/ data/ config/
